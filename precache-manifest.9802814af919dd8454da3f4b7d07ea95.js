26c3ae5764967f0497a96a0ec29060b19a1dd82d
Harshita shree <harshita360@gmail.com>
2020-08-26 22:05:46 +0530

    Updates

---
+++ precache-manifest.9802814af919dd8454da3f4b7d07ea95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78cf1d2a271341b4d95fffda88af17b3",
+    "url": "/NittyGames/index.html"
+  },
+  {
+    "revision": "2031d7be2a130321e2be",
+    "url": "/NittyGames/static/css/main.35d825b2.chunk.css"
+  },
+  {
+    "revision": "71551fecd4db0f0a01d9",
+    "url": "/NittyGames/static/js/2.156e931e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NittyGames/static/js/2.156e931e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2031d7be2a130321e2be",
+    "url": "/NittyGames/static/js/main.9435f7c8.chunk.js"
+  },
+  {
+    "revision": "eca01dfa030fa61347d2",
+    "url": "/NittyGames/static/js/runtime-main.f28bc2a7.js"
+  },
+  {
+    "revision": "66d90be8e3fb59ed4dd90aaf3cadd35f",
+    "url": "/NittyGames/static/media/background.66d90be8.jpeg"
+  }
+]);
